2b67fb0b8cbb43ba4195cba3e2d06680233cba60
igrekus <upload234@mail.ru>
2020-07-22 16:54:55 +0300

    MISC: add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='pcan_wrapper',
+    version='0.0.1',
+    author='igrekus',
+    author_email='',
+    description='Simple wrapper around PEAK-System Technik GmbH CAN SDK',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/igrekus/pcan_wrapper',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
